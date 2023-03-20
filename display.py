1baf0738eade0961fd3e177425f772b8c1d4c774
Fensoa <hugo.andriamaromanana@laplateforme.io>
2023-03-20 09:48:29 +0100

    [ADD] Init colors

---
+++ display.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import *
+from colors import COLORS
+
+screen=pygame.display.set_mode((640,480))
+DISPLAYSURF=pygame.display.set_mode((640,480))
+
+
+
+def display_login():
